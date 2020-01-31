f6d0d8c7ffbab37e46b20e5f42b83182722eaa6f
Daniel Bennett <danielbrianbennett@gmail.com>
2020-01-31 17:05:49 -0500

    Add PANAS

---
+++ questionnaires/panas/README.md
@@ -0,0 +1,6 @@
+# ius12
+
+jsPsych code for the Positive and Negative Affect Schedule (PANAS).
+
+## References
+- Watson, D., Clark, L. A., & Tellegan, A. (1988). Development and validation of brief measures of positive and negative affect: The PANAS scales. *Journal of Personality and Social Psychology, 54*(6), 1063–1070.
---
+++ questionnaires/panas/survey-panas.js
@@ -0,0 +1,219 @@
+/**
+ * survey-panas
+ * a jspsych plugin for the GAD-7
+ */
+
+jsPsych.plugins['survey-panas'] = (function() {
+
+  var plugin = {};
+
+  plugin.info = {
+    name: 'survey-panas',
+    description: '',
+    parameters: {
+      randomize_question_order: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Randomize Question Order',
+        default: true,
+        description: 'If true, the order of the questions will be randomized'
+      },
+      button_label: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Button label',
+        default:  'Continue',
+        description: 'The text that appears on the button to finish the trial.'
+      },
+    }
+  }
+  plugin.trial = function(display_element, trial) {
+
+    //---------------------------------------//
+    // Define panas questionnaire.
+    //---------------------------------------//
+
+    // Define panas items.
+    var items = [
+      "Interested",
+      "Distressed",
+      "Excited",
+      "Upset",
+      "Strong",
+      "Guilty",
+      "Scared",
+      "Hostile",
+      "Enthusiastic",
+      "Proud",
+      "Irritable",
+      "Alert",
+      "Ashamed",
+      "Inspired",
+      "Nervous",
+      "Determined",
+      "Attentive",
+      "Jittery",
+      "Active",
+      "Afraid"
+    ];
+
+    // Define panas response scale.
+    var scale = ["Very slightly or<br>not at all",
+                 "A little",
+                 "Moderately",
+                 "Quite a bit",
+                 "Extremely"
+               ];
+
+    // Randomize question order.
+    var item_order = [];
+    for(var i=0; i<items.length; i++){
+       item_order.push(i);
+    }
+    if(trial.randomize_question_order){
+       item_order = jsPsych.randomization.shuffle(item_order);
+    }
+
+    //---------------------------------------//
+    // Define survey HTML.
+    //---------------------------------------//
+
+    // Initialize HTML
+    var html = '';
+
+    // Add instructions.
+    html += '<p style="font-size:17px;">This scale consists of a number of words that describe different feelings and emotions.<br>Indicate to what extent you feel this way right now, that is, at the present moment.<p>';
+
+    // Begin form.
+    html += '<form id="jspsych-survey-panas">';
+
+    // Initialize survey container.
+    html += '<div class="panas-container">';
+
+    // Iteratively add items.
+    for (var i = 0; i < items.length; i++) {
+
+      // Add response headers (every seven items).
+      if (i % 7 == 0) {
+        html += '<div class="panas-header"></div>';
+        for (var j = 0; j < scale.length; j++) {
+          html += `<div class="panas-header">${scale[j]}</div>`;
+        }
+      }
+
+      // Initialize row.
+      html += '<div class="row-wrapper">';
+
+      // Define item number.
+      var num = ("0" + `${item_order[i]}`).slice(-2);
+
+      // Display prompt.
+      html += `<div class='panas-prompt'>${items[item_order[i]]}</div>`;
+
+      // Display responses.
+      index = [1,2,3,4,5];
+      for (let j of index) {
+        html += `<div class='panas-resp'><input type="radio" name="panas-Q${num}" value="${j}" required></div>`;
+      }
+
+      // End row.
+      html += '</div>';
+
+    }
+
+    // End survey container.
+    html += '</div>';
+
+    // Add submit button
+    html += `<div class="panas-footer"><input type="submit" id="jspsych-survey-panas" value="${trial.button_label}"></input></div>`;
+
+    // End form
+    html += '</form>'
+
+    // Display HTML
+    display_element.innerHTML = html;
+
+    //---------------------------------------//
+    // Response handling.
+    //---------------------------------------//
+
+    display_element.querySelector('#jspsych-survey-panas').addEventListener('submit', function(event) {
+
+        // Wait for response
+        event.preventDefault();
+
+        // Measure response time
+        var endTime = performance.now();
+        var response_time = endTime - startTime;
+
+        var question_data = serializeArray(this);
+        question_data = objectifyForm(question_data);
+
+        // Store data
+        var trialdata = {
+          "rt": response_time,
+          "panas": question_data
+        };
+
+        // Update screen
+        display_element.innerHTML = '';
+
+        // Move onto next trial
+        jsPsych.finishTrial(trialdata);
+
+    });
+
+    var startTime = performance.now();
+
+  };
+
+  /*!
+   * Serialize all form data into an array
+   * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+   * @param  {Node}   form The form to serialize
+   * @return {String}      The serialized form data
+   */
+  var serializeArray = function (form) {
+    // Setup our serialized data
+    var serialized = [];
+
+    // Loop through each field in the form
+    for (var i = 0; i < form.elements.length; i++) {
+      var field = form.elements[i];
+
+      // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+      if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+      // If a multi-select, get all selections
+      if (field.type === 'select-multiple') {
+        for (var n = 0; n < field.options.length; n++) {
+          if (!field.options[n].selected) continue;
+          serialized.push({
+            name: field.name,
+            value: field.options[n].value
+          });
+        }
+      }
+
+      // Convert field data to a query string
+      else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+        serialized.push({
+          name: field.name,
+          value: field.value
+        });
+      }
+    }
+
+    return serialized;
+  };
+
+  // from https://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
+  function objectifyForm(formArray) {//serialize data function
+    var returnArray = {};
+    for (var i = 0; i < formArray.length; i++){
+      returnArray[formArray[i]['name']] = formArray[i]['value'];
+    }
+    return returnArray;
+  }
+
+  return plugin;
+
+})();
