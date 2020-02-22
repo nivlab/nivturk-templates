92af435fba177a3e0e529d1a3815166f60a9ebcf
Daniel Bennett <danielbrianbennett@gmail.com>
2020-02-21 17:04:40 -0800

    Add RSL task demo

---
+++ tasks/rsl/static/js/turk-rsl-stimuli.js
@@ -0,0 +1,19 @@
+var stimulus_images = [
+  '../static/img/flag01.png',
+  '../static/img/flag02.png',
+  '../static/img/flag03.png',
+  '../static/img/flag04.png'
+];
+
+var feedback_images = [
+  '../static/img/zero_apples.png',
+  '../static/img/one_apple.png',
+  '../static/img/two_apples.png'
+];
+
+
+var slider_images = [
+  '../static/img/slider_track.png',
+  '../static/img/slider_thumb_unselected.png',
+  '../static/img/slider_thumb_selected.png'
+];
