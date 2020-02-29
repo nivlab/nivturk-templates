d185b51e662b35d7e2b517584add3c243eab37b1
Daniel Bennett <danielbrianbennett@gmail.com>
2020-02-29 14:49:32 -0500

    Add SHAPS

---
+++ questionnaires/shaps/README.md
@@ -0,0 +1,6 @@
+# SHAPS
+
+jsPsych code for the Snaith-Hamilton Pleasure Scale (Snaith et al., 1995). Minor edits were made to several questions (\#1 and \#8) to adjust unfamiliar British connotations of words for an American sample.
+
+## References
+- Snaith, R. P., Hamilton, M., Morley, S., Humayan, A., Hargreaves, D., & Trigwell, P. (1995). A scale for the assessment of hedonic tone the Snaith–Hamilton Pleasure Scale. The British Journal of Psychiatry, 167(1), 99-103.
---
+++ questionnaires/shaps/survey-shaps.js
@@ -0,0 +1,306 @@
+/**
+ * survey-shaps
+ * a jspsych plugin for the SHAPS (Snaith et al., 1995)
+ * Several minor terminology edits have been made (question #1 and question #8) to adjust British English for an American sample
+ */
+
+jsPsych.plugins['survey-shaps'] = (function() {
+
+  var plugin = {};
+
+  plugin.info = {
+    name: 'survey-shaps',
+    description: '',
+    parameters: {
+      randomize_question_order: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Randomize Question Order',
+        default: true,
+        description: 'If true, the order of the questions will be randomized'
+      },
+      button_label: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Button label',
+        default:  'Continue',
+        description: 'The text that appears on the button to finish the trial.'
+      },
+    }
+  }
+  plugin.trial = function(display_element, trial) {
+
+    //---------------------------------------//
+    // Define SHAPS questionnaire.
+    //---------------------------------------//
+
+    // Define SHAPS items
+    var items = [
+
+      "I would enjoy my favorite television or radio show",                                                       // Q1; lightly edited
+      "I would enjoy being with my family or close friends",                                                      // Q2
+      "I would find pleasure in my hobbies and pastimes",                                                         // Q3
+      "I would be able to enjoy my favourite meal",                                                               // Q4
+      "I would enjoy a warm bath or refreshing shower",                                                           // Q5
+      "I would find pleasure in the scent of flowers or the smell of a fresh sea breeze or freshly baked bread",  // Q6
+      "I would enjoy seeing other people's smiling faces",                                                        // Q7
+      "I would enjoy looking good when I have made an effort with my appearance",                                 // Q8; lightly edited
+      "I would enjoy reading a book, magazine, or newspaper",                                                     // Q9
+      "I would enjoy a cup of tea or coffee or my favorite drink",                                                // Q10
+      "I would be able to take pleasure in small things, e.g. bright sunny day, a telephone call from a friend",  // Q11
+      "I would be able to enjoy a beautiful landscape or view",                                                   // Q12
+      "I would get pleasure from helping others",                                                                 // Q13
+      "I would feel pleasure when I receive praise from other people"                                             // Q14
+
+    ];
+
+    // Define shaps response scale.
+    var scale = [
+
+       "Strongly<br>disagree",               // scored as 0
+       "Disagree",                           // scored as 1
+       "Agree",                              // scored as 2
+       "Strongly<br>agree"                   // scored as 3
+
+     ];
+
+    // Randomize question order.
+    var item_order = [];
+    for(var i=0; i<items.length; i++){
+       item_order.push(i);
+    }
+    if(trial.randomize_question_order){
+       item_order = jsPsych.randomization.shuffle(item_order);
+    }
+
+    //---------------------------------------//
+    // Define survey HTML.
+    //---------------------------------------//
+
+    // scroll to top of screen
+    window.scrollTo(0,0);
+
+    // Initialize HTML
+    var html = '';
+
+    // Insert CSS
+    html += `<style>
+    .shaps-container {
+      margin: auto;
+      width: 100%;
+      display: grid;
+      grid-template-columns: 42% 14.5% 14.5% 14.5% 14.5%;
+      grid-template-rows: auto;
+      background-color: #F8F8F8;
+      border-radius: 5px;
+    }
+
+    .row-wrapper {
+      display: contents;
+    }
+
+    .row-wrapper:hover div {
+      background-color: #dee8eb;
+    }
+
+    .shaps-header {
+      padding: 18px 0 0px 0;
+      text-align: center;
+      font-size: 13px;
+      line-height: 1.15em;
+    }
+
+    .shaps-prompt {
+      padding: 12px 0 12px 15px;
+      text-align: left;
+      font-size: 14px;
+      line-height: 1.15em;
+      justify-items: center;
+    }
+
+    .shaps-resp {
+      padding: 12px 0 12px 0;
+      font-size: 12px;
+      text-align: center;
+      line-height: 1.15em;
+      justify-items: center;
+    }
+
+    .shaps-resp input {
+      position: relative;
+    }
+
+    .shaps-resp input:after {
+        display: block;
+        content: " ";
+        position: absolute;
+        bottom: 6px;
+        background: #d8dcd6;
+        height: 2px;
+        left: 13px;
+        width: 96px;
+    }
+
+    .shaps-resp:last-child input:after {
+      display: none;
+    }
+
+    .shaps-footer {
+        margin: auto;
+        top: 95%;
+        width: 100%;
+        padding: 0 0 0 0;
+        background-color: #fff;
+        text-align: right;
+    }
+
+    /* Style the submit button */
+    .shaps-footer input[type=submit] {
+      background-color: #F0F0F0;
+      color: black;
+      padding: 8px 20px;
+      border: none;
+      border-radius: 4px;
+      float: center;
+      margin-top: 5px;
+      margin-bottom: 20px;
+      margin-right: 0px;
+    }
+    </style>`;
+
+    // Add instructions.
+    html += '<p style="font-size:17px;max-width:1000px;">This questionnaire is designed to measure your ability to experience pleasure <i> in the last few days.</i></p>';
+
+    // Begin form.
+    html += '<form id="jspsych-survey-shaps">';
+
+    // Initialize survey container.
+    html += '<div class="shaps-container">';
+
+    // Iteratively add items.
+    for (var i = 0; i < items.length; i++) {
+
+      // Add response headers (every seven items).
+      if (i % 7 == 0) {
+        html += '<div class="shaps-header"></div>';
+        for (var j = 0; j < scale.length; j++) {
+          html += `<div class="shaps-header">${scale[j]}</div>`;
+        }
+      }
+
+      // Initialize row.
+      html += '<div class="row-wrapper">';
+
+      // Define item number.
+      var num = ("0" + `${item_order[i]}`).slice(-2);
+
+      // Display prompt.
+      html += `<div class='shaps-prompt'>${items[item_order[i]]}</div>`;
+
+      // Display responses.
+      index = [0,1,2,3];
+      for (let j of index) {
+        html += `<div class='shaps-resp'><input type="radio" name="shaps-Q${num}" value="${j}" required></div>`;
+      }
+
+      // End row.
+      html += '</div>';
+
+    }
+
+    // End survey container.
+    html += '</div>';
+
+    // Add submit button
+    html += `<div class="shaps-footer"><input type="submit" id="jspsych-survey-shaps" value="${trial.button_label}"></input></div>`;
+
+    // End form
+    html += '</form>'
+
+    // Display HTML
+    display_element.innerHTML = html;
+
+    //---------------------------------------//
+    // Response handling.
+    //---------------------------------------//
+
+    display_element.querySelector('#jspsych-survey-shaps').addEventListener('submit', function(event) {
+
+        // Wait for response
+        event.preventDefault();
+
+        // Measure response time
+        var endTime = performance.now();
+        var response_time = endTime - startTime;
+
+        var question_data = serializeArray(this);
+        question_data = objectifyForm(question_data);
+
+        // Store data
+        var trialdata = {
+          "rt": response_time,
+          "shaps": question_data
+        };
+
+        // Update screen
+        display_element.innerHTML = '';
+
+        // Move onto next trial
+        jsPsych.finishTrial(trialdata);
+
+    });
+
+    var startTime = performance.now();
+
+  };
+
+  /*!
+   * Serialize all form data into an array
+   * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+   * @param  {Node}   form The form to serialize
+   * @return {String}      The serialized form data
+   */
+  var serializeArray = function (form) {
+    // Setup our serialized data
+    var serialized = [];
+
+    // Loop through each field in the form
+    for (var i = 0; i < form.elements.length; i++) {
+      var field = form.elements[i];
+
+      // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+      if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+      // If a multi-select, get all selections
+      if (field.type === 'select-multiple') {
+        for (var n = 0; n < field.options.length; n++) {
+          if (!field.options[n].selected) continue;
+          serialized.push({
+            name: field.name,
+            value: field.options[n].value
+          });
+        }
+      }
+
+      // Convert field data to a query string
+      else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+        serialized.push({
+          name: field.name,
+          value: field.value
+        });
+      }
+    }
+
+    return serialized;
+  };
+
+  // from https://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
+  function objectifyForm(formArray) {//serialize data function
+    var returnArray = {};
+    for (var i = 0; i < formArray.length; i++){
+      returnArray[formArray[i]['name']] = formArray[i]['value'];
+    }
+    return returnArray;
+  }
+
+  return plugin;
+
+})();
